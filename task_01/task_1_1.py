b7d8817399eda76d009e1cf71293c807dbb749b9
Aleksandr Polevoy <polevoyaa@gmail.com>
2022-11-27 22:50:52 +0300

    init

---
+++ task_01/task_1_1.py
@@ -0,0 +1,20 @@
+import os
+
+os.system('cls')
+
+def enter_dey_of_week():
+    dey_of_week = input('Введите день недели числом от 1 до 7  ')
+    if (not dey_of_week.isdigit()):
+        print ('Вы ввели что-то не то. Попробуйте еще раз')
+        return enter_dey_of_week()
+    dey_of_week = int(dey_of_week)
+    if(dey_of_week < 1 or dey_of_week > 7):
+        print('День недели должен быть от 1 до 7. Попробуйте еще раз')
+        return enter_dey_of_week()
+    return dey_of_week
+#print('Введите день недели числом от 1 до 7  ')
+dey_of_week = enter_dey_of_week()
+if(5<dey_of_week<8):
+    print(f'{dey_of_week} -> выходной день')
+else:
+    print(f'{dey_of_week} -> рабочий день')
